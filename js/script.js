fa07bb4ae42820e7c9fd91fa56c1d6efbdb97421
Surya <surya@surya-bd-c02s576eg8wn.attlocal.net>
2017-12-14 22:47:13 -0600

    nav changes as per xl

--- js/script.js
+++ js/script.js
@@ -1,25 +1,67 @@
 document.getElementById("navMenu").innerHTML =
         '<ul class="styleguide-navigation">'+
-            '<li><a href="./form-controls.html">Form Controls</a></li>'+
-            '<li><a href="./buttons.html">Buttons</a></li>'+
-            '<li><a href="./tabs.html" >Card Navigation</a></li>'+
-            '<li><a href="./vertical-nav.html">Vertical Navigation</a></li>'+
-            '<li><a href="./message-toast.html">Message - Toast</a></li>'+
-            '<li><a href="./message-contextual.html">Message - Contextual</a></li>'+
-            '<li><a href="./message-modal.html">Message - Modal</a></li>'+
-            '<li><a href="./selection-multiselect.html">Selection - Multiselect</a></li>'+
-            '<li><a href="./activitynprogress.html">Activity and Progress</a></li>'+
-            '<li><a href="./Pagination.html">Pagination</a></li>'+
-            '<li><a href="./data-list.html">Data list</a></li>'+
-            '<li><a href="./filters.html">Filters</a></li>'+
-            '<li><a href="./taskpage-def.html">Task page Default</a></li>'+
-            '<li><a href="./tooltip.html">Tooltip</a></li>'+
-            '<li><a href="./help-form.html">Help form</a></li>'+
-            '<li><a href="./partner-ribbon.html">Partner Ribbon</a></li>'+
-            '<li><a href="./data-grid.html">Data Grid</a></li>'+
-            '<li><a href="./transaction-form.html">Transaction form</a></li>'+
-            '<li><hr/></li>'+
+            // '<li><a href="./form-controls.html">Form Controls</a></li>'+
+            // '<li><a href="./buttons.html">Buttons</a></li>'+
+            // '<li><a href="./tabs.html" >Card Navigation</a></li>'+
+            // '<li><a href="./vertical-nav.html">Vertical Navigation</a></li>'+
+            // '<li><a href="./message-toast.html">Message - Toast</a></li>'+
+            // '<li><a href="./message-contextual.html">Message - Contextual</a></li>'+
+            // '<li><a href="./message-modal.html">Message - Modal</a></li>'+
+            // '<li><a href="./selection-multiselect.html">Selection - Multiselect</a></li>'+
+            // '<li><a href="./activitynprogress.html">Activity and Progress</a></li>'+
+            // '<li><a href="./Pagination.html">Pagination</a></li>'+
+            // '<li><a href="./data-list.html">Data list</a></li>'+
+            // '<li><a href="./filters.html">Filters</a></li>'+
+            // '<li><a href="./taskpage-def.html">Task page Default</a></li>'+
+            // '<li><a href="./tooltip.html">Tooltip</a></li>'+
+            // '<li><a href="./help-form.html">Help form</a></li>'+
+            // '<li><a href="./partner-ribbon.html">Partner Ribbon</a></li>'+
+            // '<li><a href="./data-grid.html">Data Grid</a></li>'+
+            // '<li><a href="./transaction-form.html">Transaction form</a></li>'+
+            // '<li><hr/></li>'+
             '<li class="framwork-components"><ul>Framework Components'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Form Controls'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Selection Control'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Misc'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Data Pattern'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Messaging'+
+                '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
+                '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
+                '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
+                '<li class="component-library"><a href="./framework components/help-support.html">Help & Support</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '<li class="framwork-components"><ul>Search Components'+
                 '<li class="component-library"><a href="./framework components/left-nav.html">Left Navigation</a></li>'+
                 '<li class="component-library"><a href="./framework components/tool-bar.html">Tool bar</a></li>'+
                 '<li class="component-library"><a href="./framework components/task-panel.html">Task Panel</a></li>'+
