634d0f3cd67da69b6e8305cd8f32c5d0a075a65e
Surya <>
2017-12-18 08:42:14 -0600

    changes merged

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
+function enableTxt(elem) {
+    var id = $(elem).attr("id");
+    alert(id);
+}
+
 document.getElementById("navMenu").innerHTML =
         '<ul class="styleguide-navigation">'+
-            // '<li><a href="./form-controls.html">Form Controls</a></li>'+
-            // '<li><a href="./buttons.html">Buttons</a></li>'+
-            // '<li><a href="./tabs.html" >Card Navigation</a></li>'+
-            // '<li><a href="./vertical-nav.html">Vertical Navigation</a></li>'+
-            // '<li><a href="./message-toast.html">Message - Toast</a></li>'+
-            // '<li><a href="./message-contextual.html">Message - Contextual</a></li>'+
-            // '<li><a href="./message-modal.html">Message - Modal</a></li>'+
-            // '<li><a href="./selection-multiselect.html">Selection - Multiselect</a></li>'+
-            // '<li><a href="./activitynprogress.html">Activity and Progress</a></li>'+
-            // '<li><a href="./Pagination.html">Pagination</a></li>'+
-            // '<li><a href="./data-list.html">Data list</a></li>'+
-            // '<li><a href="./filters.html">Filters</a></li>'+
-            // '<li><a href="./taskpage-def.html">Task page Default</a></li>'+
-            // '<li><a href="./tooltip.html">Tooltip</a></li>'+
-            // '<li><a href="./help-form.html">Help form</a></li>'+
-            // '<li><a href="./partner-ribbon.html">Partner Ribbon</a></li>'+
-            // '<li><a href="./data-grid.html">Data Grid</a></li>'+
-            // '<li><a href="./transaction-form.html">Transaction form</a></li>'+
-            // '<li><hr/></li>'+
+            
             
             '<li class="framwork-components"><ul>Framework Components'+
-                '<li class=""><a href="./tool-bar.html">Toolbar</a></li>'+
-                '<li class=""><a href="./Breadcrumbs.html">Breadcrumbs</a></li>'+
+                '<li><a id="toolbar" href="./tool-bar.html" onclick=menuClick(this.id)>Toolbar</a></li>'+
                 '<li class=""><a href="./left-nav.html">Navigation-Primary (Left Nav)</a></li>'+
+                '<li class=""><a href="./help-support.html">Help & Support</a></li>'+
+                '<li class=""><a href="./task-panel.html">Task/Notification Panel</a></li>'+
+                '<li class=""><a href="./Breadcrumbs.html">Breadcrumbs</a></li>'+
                 '<li class=""><a href="./tabs.html">Navigation-Secondary (Tabs)</a></li>'+
                 '<li class=""><a href="./vertical-nav.html">Navigation-Vertical</a></li>'+
-                '<li class=""><a href="./help-support.html">Help & Support</a></li>'+
-                '<li class=""><a href="./form-controls.html">Form and Templates Panel</a></li>'+
-                
-                '<li class=""><a href="./task-panel.html">Task/Notification Panel</a></li>'+
+                '<li class=""><a href="./inprogress.html">Form and Templates Panel</a></li>'+
                 '<li class=""><a href="./header-ribbon.html">Header Ribbon</a></li>'+
-                '<li class=""><a href="./footer-wAction.html">Footer w/ Actions (old lightbox model)</a></li>'+
+                '<li class=""><a href="./footer-wAction.html">Footer w/ Actions</a></li>'+
                 '<li class=""><a href="./cards.html">Cards- different types of cards</a></li>'+
                 '<li class=""><a href="./partner-ribbon.html">Timeline</a></li>'+
-                '<li class=""><a href="#">Project Cycle</a></li>'+
-                '<li class=""><a href="#">Bulk Edit</a></li>'+
+                '<li class=""><a href="./inprogress.html">Project Cycle</a></li>'+
+                '<li class=""><a href="./inprogress.html">Bulk Edit</a></li>'+
                 '<li class=""><a href="./filters.html">Filters</a></li>'+
-                '<li class=""><a href="#">Lightbox</a></li>'+
+                '<li class=""><a href="./inprogress.html">Lightbox</a></li>'+
                 '</ul>'+
             '</li>'+
             '<li class="framwork-components"><ul>Form Controls'+
@@ -47,12 +33,12 @@ document.getElementById("navMenu").innerHTML =
                 '<li class=""><a href="./check-box.html">Check Box</a></li>'+
                 '<li class=""><a href="./buttons.html">Buttons</a></li>'+
                 '<li class=""><a href="./input-field.html">Input fields (inc scalable text field)</a></li>'+
-                '<li class=""><a href="#">Links (Actionable links)</a></li>'+
+                '<li class=""><a href="./inprogress.html">Links (Actionable links)</a></li>'+
                 '<li class=""><a href="./toggle-switch.html">Toggle / Switch</a></li>'+
                 '</ul>'+
             '</li>'+
             '<li class="framwork-components"><ul>Selection Control'+
-                '<li class=""><a href="#">Dropdown</a></li>'+
+                '<li class=""><a href="./dropdown.html">Dropdown</a></li>'+
                 '<li class=""><a href="./selection-multiselect.html">Multi select dropdown</a></li>'+
                 '</ul>'+
             '</li>'+
@@ -63,8 +49,8 @@ document.getElementById("navMenu").innerHTML =
             '</li>'+
             '<li class="framwork-components"><ul>Data Pattern'+
                 '<li class=""><a href="./data-list.html">List style for pages</a></li>'+
-                '<li class=""><a href="#">List style for cards</a></li>'+
-                '<li class=""><a href="#">Charts</a></li>'+
+                '<li class=""><a href="./inprogress.html">List style for cards</a></li>'+
+                '<li class=""><a href="./inprogress.html">Charts</a></li>'+
                 '</ul>'+
             '</li>'+
             '<li class="framwork-components"><ul>Messaging'+
@@ -74,10 +60,10 @@ document.getElementById("navMenu").innerHTML =
                 '<li class=""><a href="./message-modal.html">Modal</a></li>'+
                 '</ul>'+
             '</li>'+
-            '<li class="framwork-components"><ul>Search Components'+
-                '<li class=""><a href="#">Search- type ahead interactions</a></li>'+
-                '<li class=""><a href="#">Search results- card view, list view</a></li>'+
-                '<li class=""><a href="#">Advanced Search</a></li>'+
-                '</ul>'+
-            '</li>'+
+            // '<li class="framwork-components"><ul>Search Components'+
+            //     '<li class=""><a href="./inprogress.html">Search- type ahead interactions</a></li>'+
+            //     '<li class=""><a href="./inprogress.html">Search results- card view, list view</a></li>'+
+            //     '<li class=""><a href="./inprogress.html">Advanced Search</a></li>'+
+            //     '</ul>'+
+            // '</li>'+
         '</ul>';
